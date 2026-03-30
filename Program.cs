a927b2ab98810664546f57009aeeefc85eb621ed
Mostafatahadev1 <mostafatahadev1@gmail.com>
2026-03-30 22:48:04 +0200

    Question 5

--- Program.cs
+++ Program.cs
@@ -92,6 +92,26 @@
 
             #endregion
 
+            #region Question 5
+            // Find Max Value in an Array using Generics
+
+            // method returns the maximum value between two values
+            // we use constraint IComparable<T> so we can compare values
+
+            /*
+             public static T Max<T>(T a, T b) where T : IComparable<T>
+                {
+                    return a.CompareTo(b) > 0 ? a : b;
+                }
+             */
+
+            // int max = FindMax(100, 200);
+            // Console.WriteLine(max);  200
+
+            #endregion
+
+
+
 
         }
     }
