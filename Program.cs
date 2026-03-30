07fa1b0728dd8c550485801e9f6587ee9211065c
Mostafatahadev1 <mostafatahadev1@gmail.com>
2026-03-30 22:32:52 +0200

    Question 2

--- Program.cs
+++ Program.cs
@@ -5,18 +5,43 @@
         static void Main(string[] args)
         {
             #region Question 1
-            // Generic Class:
-            // A generic class is a class that uses a type placeholder (like T)
-            // instead of specifying a fixed data type.
-            // This allows the same class to work with different data types (int, string, etc.)
+        // Generic Class:
+        // A generic class is a class that uses a type placeholder (like T)
+        // instead of specifying a fixed data type.
+        // This allows the same class to work with different data types (int, string, etc.)
 
-            // Why use Generics?
-            // 1- Type Safety: prevents type errors at compile-time instead of runtime
-            // 2- Performance: avoids boxing/unboxing when using value types
-            // 3- Code Reuse: write one class and use it with multiple data types
-            // 4- Cleaner Code: makes code more readable and maintainable
+        // Why use Generics?
+        // 1- Type Safety: prevents type errors at compile-time instead of runtime
+        // 2- Performance: avoids boxing/unboxing when using value types
+        // 3- Code Reuse: write one class and use it with multiple data types
+        // 4- Cleaner Code: makes code more readable and maintainable
 
             #endregion
+
+
+            #region Question 2
+
+        //  class Container<T>
+        //   {
+        //  private T item;
+        //
+        //  public void Add(T value)
+        //  {
+        //      item = value;
+        //  }
+        //
+        //  public T Get()
+        //  {
+        //      return item;
+        //  }
+        //  }
+
+        // Container<int> c = new Container<int>();
+            // c.Add(10);
+            // Console.WriteLine(c.Get()); // 10
+            #endregion
+
+
         }
     }
 }
